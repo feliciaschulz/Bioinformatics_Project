1663dd6d680e4337fa8c5fd19a79fe0cb2dcccf0
fschul <72020518+fschul@users.noreply.github.com>
2023-05-31 22:41:37 +0200

    Updating survival script

--- 02_Survival_Analysis/survival_analysis.R
+++ 02_Survival_Analysis/survival_analysis.R
@@ -1,3 +1,7 @@
+### Felicia Schulz
+### This script creates survival plots for two different .csv files
+### The input data must be a merged PDL1 output scores and classes file as well
+### as a survival file
 rm(list=ls())
 library(survminer)
 require("survival")
@@ -10,24 +14,9 @@ survival_data$OS <- survival_data$Include_ACT_OS_Analysis
 survival_data$Include_ACT_OS_Analysis <- NULL
 
 
-
-### don't need these two ###
-# plot with only OS positive
-fitx <- survfit(Surv(IDFS, IDFSbin) ~ 1, data = df)
-ggsurvplot(fitx, conf.int = TRUE, risk.table = "abs_pct", risk.table.y.text.col = TRUE, censor = TRUE, title = "Survival plot H&E stained data\nInclude_ACT_OS_Analysis=1")
-# can't add median survival because survival > 0.5 at all times
-# plot with both OS=1 and OS=2
-fity <- survfit(Surv(IDFS, IDFSbin) ~ OS, data = survival_data)
-ggsurvplot(fity, pval = TRUE, linetype = "strata", conf.int = TRUE, risk.table = TRUE, risk.table.y.text.col = TRUE,
-           title = "Survival plot H&E stained data")
-
-
-### 1. Plot with cell counts (set threshold at median)
-### 2. Plot with ml predictions (threshold=0.5)
-
 #merged <- read.csv("merged_data.csv")
-merged_he <- read.csv("../05_Comparisons_PDL1_Performance/merged_HEstained.csv")
-merged_ihc <- read.csv("../05_Comparisons_PDL1_Performance/merged_PDL1stained.csv")
+merged_he <- read.csv("../01_PDL1_Performance_Analysis/merged_HEstained.csv")
+merged_ihc <- read.csv("../01_PDL1_Performance_Analysis/merged_PDL1stained.csv")
 surv_merged_he <- inner_join(df, merged_he, by = "PD_ID")
 surv_merged_ihc <- inner_join(df, merged_ihc, by="PD_ID")
 
@@ -63,24 +52,38 @@ plot4 <- ggsurvplot(fit4, conf.int=TRUE, pval=TRUE, linetype = "strata", risk.ta
            title = "Survival plot TNBC data divided by ML model\npredictions of presence of PDL1 in IHC-stained data")
 
 
-#grid.arrange(plot1, plot2, plot3, plot4, nrow=2, ncol=2)
+# To get the output as a .jpg image with all four plots, run this below section
+# Otherwise, just print the plots by running "plot4", and then save them manually.
+
 plotlist <- list()
+# The "ggsurvplot" functions here can also be replaced with the "plot1", "plot2" etc
+# defined earlier. Here I redefined them so that the captions and styles could be altered
+# so that in the grouping, it looks less messy.
 plotlist[[1]] <- ggsurvplot(fit3, conf.int=TRUE, pval=TRUE, linetype = "strata", risk.table = TRUE, risk.table.y.text.col = TRUE,
                             legend.title = "ML prediction", legend.labs = c("0", "1"),
                             title = "H&E-stained data",
-                            ggtheme = theme_minimal())
+                            ggtheme = theme_grey())
 
 plotlist[[2]] <-  ggsurvplot(fit4, conf.int=TRUE, pval=TRUE, linetype = "strata", risk.table = TRUE, risk.table.y.text.col = TRUE,
                              legend.title = "ML prediction", legend.labs = c("0", "1"),
                              title = "IHC-stained data",
                              ggtheme = theme_grey())
 
-#plotlist[[3]] <- plot3
-#plotlist[[4]] <- plot4
+plotlist[[3]] <- ggsurvplot(fit1, conf.int = TRUE, pval=TRUE, risk.table = TRUE, risk.table.y.text.col = TRUE,
+                            legend.title = "Cell counts", legend.labs = c("high", "low"), 
+                            title = "Cell counts",
+                            ggtheme = theme_grey())
 
-res <- arrange_ggsurvplots(plotlist, print=FALSE, ncol=2, nrow=1, risk.table.height = 0.3, 
+plotlist[[4]] <- ggsurvplot(fit2, conf.int=TRUE, pval=TRUE, linetype = "strata", risk.table = TRUE, risk.table.y.text.col = TRUE,
+                            legend.title = "Pathologist label", legend.labs = c("0", "1"),
+                            title = "Pathologist's labels",
+                            ggtheme = theme_grey())
+
+# Arrange all plots into one
+res <- arrange_ggsurvplots(plotlist, print=FALSE, ncol=2, nrow=2, risk.table.height = 0.3, 
                            title = "Survival plot TNBC data divided by ML model predictions of presence of PDL1")
-ggsave("myfile.pdf", res, width=10, height=5, units="in")
+# Save plots
+ggsave("all_survival_plots.png", res, device="png", width=12, height=12, units="in")
 
 
 
