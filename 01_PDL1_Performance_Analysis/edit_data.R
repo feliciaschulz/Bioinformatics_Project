883ce093defb83e7aeaf9f8560502b81b21b20e8
fschul <72020518+fschul@users.noreply.github.com>
2023-05-31 22:24:02 +0200

    Updating edit_data.R script

--- 01_PDL1_Performance_Analysis/edit_data.R
+++ 01_PDL1_Performance_Analysis/edit_data.R
@@ -1,15 +1,20 @@
-# this code edits and merges the two dfs output.txt and PDL1scores.csv and saves
-# them as the new df merged_data.csv to the directory 06
+### Felicia Schulz
+### This code edits and merges the two data frames output.txt and PDL1scores.csv and saves
+### them as the new data frame mergedPDL1stained.csv 
 
+# Load ML data
 ml_results <- read.csv("../05_Comparisons_PDL1_Performance/output.txt", sep=" ", header=FALSE)
 colnames(ml_results) <- c("Name", "PDL1score_sp142")
+# Add class
 ml_results$PDL1class_sp142 <- ifelse(ml_results$PDL1score_sp142 >= 0.5, 1, 0)
+# Standardize names
 ml_results$Name <- gsub(".*/(TNBC[^/]*)\\.jpg:.*", "\\1", ml_results$Name)
 
-
+# Load pathologist data
 pathologist_data <- read.csv("../05_Comparisons_PDL1_Performance/PDL1scores.csv")
+# Standardize names
 pathologist_data$Name <- gsub("\\.png|\\.jpg| ", "\\1", pathologist_data$Name)
-pathologist_data <- drop_na(pathologist_data)
+pathologist_data <- drop_na(pathologist_data) # remove NA patients
 
 # merge the data sets based on the "Name" column
 merged <- inner_join(ml_results, pathologist_data, by = "Name")
@@ -23,9 +28,9 @@ merged$score_true <- merged$score_true / 100
 merged$PD_ID <- merged$PDid
 merged$PDid <- NULL
 
-
+# Create the median column
 median(merged$PDL1_SP142) # 38.5
 merged$cell_counts_class <- ifelse(merged$PDL1_SP142>=median(merged$PDL1_SP142), "above_median", "below_median")
-write.csv(merged, "merged_HEstained.csv", row.names=FALSE, quote=FALSE) 
+write.csv(merged, "merged_PDL1stained.csv", row.names=FALSE, quote=FALSE) 
 
 
